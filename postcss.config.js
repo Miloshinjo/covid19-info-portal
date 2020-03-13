328ae492513c01009cc8b6089e57c190c3ce167f
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-13 23:34:47 +0100

    Add preset env

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: ['./components/**/*.js', './pages/**/*.js'],
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}'
+            ],
             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
           }
-        }
-      : {})
-  }
+        ]
+      : undefined,
+    'postcss-preset-env'
+  ]
 };
