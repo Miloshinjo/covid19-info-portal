6ad8a5be0c81144f4e93253f47307209e7955aab
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-17 14:58:06 +0100

    Add border top

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import CoronaIcon from '../components/icons/Corona';
 
 export default function Header() {
   return (
-    <header className="mx-auto w-full px-4 lg:px-0 lg:w-1/2 flex flex-col justify-center border-t-4 border-teal-500">
-      <div className="inline-flex items-center">
+    <header className="mx-auto flex flex-col justify-center border-t-4 border-teal-500">
+      <div className="inline-flex mx-auto items-center w-full px-4 lg:px-0 lg:w-1/2">
         <CoronaIcon className="w-10 h-10 mr-2 fill-current text-teal-500" />
         <h1 className="text-xl md:text-3xl font-semibold bold my-8">
           <span className="text-teal-500 font-bold">COVID-19</span> Info Portal
