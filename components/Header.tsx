55a2b3339482a798d9f1cd74d4635366848e58ad
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-17 15:33:42 +0100

    Add lang switcher

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,19 @@
-import CoronaIcon from '../components/icons/Corona';
+import CoronaIcon from './icons/Corona';
+import LanguageSelector from './LanguageSelector';
 
 export default function Header() {
   return (
-    <header className="mx-auto flex flex-col justify-center border-t-4 border-teal-500">
-      <div className="inline-flex mx-auto items-center w-full px-4 lg:px-0 lg:w-1/2">
-        <CoronaIcon className="w-10 h-10 mr-2 fill-current text-teal-500" />
-        <h1 className="text-xl md:text-3xl font-semibold bold my-8">
-          <span className="text-teal-500 font-bold">COVID-19</span> Info Portal
-        </h1>
+    <header className="mx-auto py-8 flex flex-col justify-center border-t-4 border-teal-500">
+      <div className="mx-auto flex items-center justify-between w-full px-4 lg:px-0 lg:w-1/2">
+        <div className="flex items-center">
+          <CoronaIcon className="w-10 h-10 mr-2 fill-current text-teal-500" />
+          <h1 className="text-xl md:text-3xl font-semibold bold">
+            <span className="text-teal-500 font-bold">COVID-19</span> Info
+            Portal
+          </h1>
+        </div>
+
+        <LanguageSelector />
       </div>
     </header>
   );
---
+++ components/LanguageSelector.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent, useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function CountrySelector() {
+  const router = useRouter();
+  const [value, setValue] = useState<string>(router.pathname.replace('/', ''));
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === 'en') {
+      router.push('/');
+      setValue(e.target.value);
+      return;
+    }
+    router.push(`/${e.target.value}`);
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className="relative md:w-1/6">
+      <select
+        onChange={handleChange}
+        className="block cursor-pointer appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-1 px-2 pr-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-sm"
+        value={value}
+      >
+        <option value="en">English</option>
+        <option value="sr">Srpski</option>
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+        <svg
+          className="fill-current h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </svg>
+      </div>
+    </div>
+  );
+}
