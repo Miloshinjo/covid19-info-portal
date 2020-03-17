ef5670d4c8fab24e96b394e72d6cd433389bd4f1
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-17 21:30:44 +0100

    Add google site verification

--- pages_/_document.tsx
+++ pages_/_document.tsx
@@ -5,6 +5,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <meta
+          name="google-site-verification"
+          content="VmFgTyGcsw8UYPSmOB6AbGkdxYxchkpKOUoftd8_pX8"
+        />
         <body className="bg-gray-100 antialiased text-gray-900">
           <Main />
           <NextScript />
