303ef4d97e8f791f85a6daccc0a00676b53bf488
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-18 14:20:38 +0100

    Add more responsivnes

--- components/NumbersCard.tsx
+++ components/NumbersCard.tsx
@@ -18,9 +18,11 @@ export default function Card({
   return (
     <div className="inline-flex flex-col justify-between elevation">
       <div className="p-4">
-        <h2 className="text-center uppercase font-medium text-lg">{title}</h2>
+        <h2 className="text-center uppercase font-medium text-lg md:text-base xl:text-lg">
+          {title}
+        </h2>
         <div
-          className={`text-center mt-3 font-medium text-4xl ${textColor} flex justify-center`}
+          className={`text-center mt-3 font-medium text-4xl md:text-2xl xl:text-4xl ${textColor} flex justify-center`}
         >
           {error ? (
             '0'
