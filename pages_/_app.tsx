9dfee2f65078011f712c06e6898dc7406069fc0b
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-18 10:09:34 +0100

    Add favicon and meta tags

--- pages_/_app.tsx
+++ pages_/_app.tsx
@@ -1,9 +1,23 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>COVID-19 Info Portal</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <meta name="description" content="COVID-19 information in one place" />
+      </Head>
+      <Component {...pageProps} />{' '}
+    </>
+  );
 }
 
 export default MyApp;
