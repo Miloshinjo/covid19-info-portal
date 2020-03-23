1454fe65eb998cb9fe972806462e4774ec5087c8
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-03-23 15:46:52 +0100

    Remove border-top

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import LanguageSelector from './LanguageSelector';
 
 export default function Header(): JSX.Element {
   return (
-    <header className="mx-auto py-8 flex flex-col justify-center border-t-4 border-teal-600">
+    <header className="mx-auto py-8 flex flex-col justify-center">
       <div className="mx-auto flex items-center justify-between w-full px-4 lg:px-0 lg:w-1/2">
         <div className="flex items-center">
           <CoronaIcon className="w-10 h-10 mr-2 fill-current text-teal-600" />
